599149143b942546e116b59217b642db87b6e624
EsAm1973 <me354172@gmail.com>
2024-11-27 20:14:08 +0200

    make the model movie and cubit api

---
+++ lib/data/repository/movie_api_repository.dart
@@ -0,0 +1,13 @@
+import 'package:movie_app/data/api/movie_api.dart';
+import 'package:movie_app/data/model/movie.dart';
+
+class MovieApiRepository {
+  final MovieService movieService;
+
+  MovieApiRepository({required this.movieService});
+
+  Future<List<Movie>> fetchTopMovie() async {
+    final jsonData = await movieService.fetchTopMovie();
+    return jsonData.map((json) => Movie.fromJson(json)).toList();
+  }
+}
